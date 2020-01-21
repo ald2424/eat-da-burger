df9bd27aa8f4aa6ca88ef48f13146ac5c3991621
ald2424 <abigaild2424@gmail.com>
2020-01-21 16:52:27 -0500

    fixed a sql.seed error

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
 INSERT INTO burgers (burger_name) VALUES ('Bacon and Cheese Burger');
 INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
-INSERT INTO burgers (burger_name) VALUES ('BBQ Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('BBQ Burger', true);
